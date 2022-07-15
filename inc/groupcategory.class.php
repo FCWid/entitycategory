8242ee81862ee97b95d973e78daa7e12b547bdde
FCWid <89387880+FCWid@users.noreply.github.com>
2022-07-15 15:04:43 +0200

    Update groupcategory.class.php

--- inc/groupcategory.class.php
+++ inc/groupcategory.class.php
@@ -61,8 +61,7 @@ class PluginEntitycategoryEntitycategory extends CommonDBTM {
         $user = new User();
 
         if ($user->getFromDB($user_id)) {
-            $user_entities = Entity_User::getUserEntities($user_id);
-
+            $user_entities = Profile_User::getUserEntities($user_id->fields['id']);
             foreach ($user_entities as $entity_data) {
                 $entity = new Entity();
                 if ($entity->getFromDB($entity_data['id'])) {
